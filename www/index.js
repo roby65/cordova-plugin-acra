0dc55ac8f6a8d079742340c67b16071ea3def683
Paul Panserrieu <paul.panserrieu@gmail.com>
2014-07-30 15:52:24 +0200

    add printStackTrace, to see what's going on in iOS

--- www/index.js
+++ www/index.js
@@ -1,23 +1,14 @@
 /*
- * send js errors to acra the ios part
+ * send js errors to acra the ios part...
  */
 
-// test default values
 var config = {
-    ACRA_SERVER : "http://192.168.0.48:9000",
-    APP_VERSION_NAME : "test-0.0.1",
+    ACRA_SERVER : window.ACRA_SERVER || "http://192.168.43.149:9000",
+    APP_VERSION_NAME : window.ACRA_SERVER || "default-0.0.1",
     APP_VERSION_CODE : 1,
-    PACKAGE_NAME : "",
+    PACKAGE_NAME : window.PACKAGE_NAME || "default-bundleId",
     ANDROID_VERSION : "ios"
 };
-
-function init(acra_server, version, bundleId) {
-    config.ACRA_SERVER = acra_server;
-    config.APP_VERSION_NAME = version;
-    config.APP_VERSION_CODE = 1;
-    config.PACKAGE_NAME = bundleId;
-    config.ANDROID_VERSION = "ios";
-}
 
 function report(err) {
     return {
@@ -26,14 +17,14 @@ function report(err) {
         ANDROID_VERSION : 'ios-' + device.version,
         CUSTOM_DATA: {
             message: err.message,
-            line: err.line || "",
-            sourceId: err.sourceId || ""
+            line: err.line || 0,
+            sourceId: err.sourceId || "none"
         },
         PHONE_MODEL: device.model,
         APP_VERSION_NAME: config.APP_VERSION_NAME,
         APP_VERSION_CODE: config.APP_VERSION_CODE,
         REPORT_ID: (Math.random() * 10000).toString() + "/" + device.uuid,
-        STACK_TRACE: err.stack || "no stack trace available " + ((Math.random() * 10000).toString())
+        STACK_TRACE: window.printStackTrace({e:err}).join(',')
     };
 }
 
@@ -50,12 +41,10 @@ function xhr(method, url, data, cb) {
     x.send(JSON.stringify(data));
 }
 
-// FIXME
 function send(err, sourceId, line) {
-    console.log(err);
     if(device.platform !== 'Android') {
         xhr('POST', config.ACRA_SERVER + '/report', report(err), function (err, response) {
-            console.log('error sent to acra');
+            console.log('error sent to acra, server response: ' + response);
         });
     }
 }
@@ -64,8 +53,5 @@ function send(err, sourceId, line) {
 window.onerror = send;
 
 module.exports = {
-    // expose send for testing
-    send:send,
-    // initialize the acra plugin
-    init:init
+    send:send
 };
